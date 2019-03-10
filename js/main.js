67b67038fd001835af58e7ea10b6f62cd4ce4a5d
Phillip Sun <psun.04@gmail.com>
2019-03-09 19:52:14 -0600

    wsd a lot of tweaks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 (function() {
   console.log("Hello");
+
+  // element argument can be a selector string
+  // for an individual element
+  var flkty = new Flickity(".hero__slider", {
+    cellAlign: "left",
+    contain: true,
+    wrapAround: true,
+    pageDots: false,
+    autoPlay: 3000
+  });
 })();
